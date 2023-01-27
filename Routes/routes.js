d80220dca60f2dfbe2818788865550da797697db
SAHNCOYS <lullullullul94@gamil.com>
2023-01-28 01:56:21 +0300

    register added

--- Routes/routes.js
+++ Routes/routes.js
@@ -11,4 +11,31 @@ router.get("/users", (req, res) => {
   });
 });
 
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  console.log(email);
+  connection.query(
+    `SELECT email FROM users WHERE email = '${email}'`,
+    (err, results) => {
+      if (!err && !results.length) {
+        connection.query(
+          `INSERT INTO users (id, name, email, password) VALUES (NULL, '${name}', '${email}', '${password}')`,
+          (err, results) => {
+            if (err) {
+              console.log(err);
+              res.json({ isErr: true });
+            } else res.json({ isSuccess: true });
+          }
+        );
+      } else if (!err && results.length) {
+        res.status(409).json({ isSameUser: true });
+      } else if (err) {
+        console.log(err);
+        res.json({ isErr: true });
+      }
+    }
+  );
+});
+
 export default router;
