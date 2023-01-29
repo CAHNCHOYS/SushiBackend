93fb138907c5c5b7f0be67f16ad2802e1ceb129a
SAHNCOYS <lullullullul94@gamil.com>
2023-01-29 14:04:14 +0300

    w

--- Routes/routes.js
+++ Routes/routes.js
@@ -18,7 +18,11 @@ router.get("/users", (req, res) => {
 router.post("/register", (req, res) => {
   console.log(req.body);
   const { name, email, password, city } = req.body;
-
+  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   connection.query(
     `SELECT email FROM users WHERE email = '${email}'`,
     (err, results) => {
