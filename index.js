d60bdb26878d70abc4e87633988d87872127ca40
SAHNCOYS <lullullullul94@gamil.com>
2023-01-29 13:47:29 +0300

    cors set vercel json

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
 
 app.use(router);
 
