f547e596ff9550ac686e5ef81a3931a9c0fd378c
SAHNCOYS <lullullullul94@gamil.com>
2023-02-01 22:02:29 +0300

    cart work pls2

--- Controllers/getUserCartProducts.js
+++ Controllers/getUserCartProducts.js
@@ -3,8 +3,8 @@ import connection from "../Database/connection.js";
 export const getUserCartProducts = (req, res) => {
   const id = +req.params.id;
   connection.query(
-      `SELECT products.product_id, products.name, products.Price, products.image, cartproducts.count FROM ((CartProducts INNER JOIN products ON CartProducts.product_id = products.product_id)
-  INNER JOIN users ON CartProducts.user_id = users.id) WHERE users.id = ${+id}`,
+      `SELECT products.product_id, products.name, products.Price, products.image, cartproducts.count FROM ((cartproducts INNER JOIN products ON CartProducts.product_id = products.product_id)
+  INNER JOIN users ON cartproducts.user_id = users.id) WHERE users.id = ${+id}`,
     (err, results) => {
       if (!err) {
         res.json(results);
