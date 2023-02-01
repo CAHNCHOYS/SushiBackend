32dbf4ff8259eb9e5b89543ab4fe9e6f90642c73
SAHNCOYS <lullullullul94@gamil.com>
2023-02-01 21:23:13 +0300

    cart2

--- Controllers/deleteCartProduct.js
+++ Controllers/deleteCartProduct.js
@@ -1,10 +1,10 @@
 import connection from "../Database/connection.js";
 
-export const deleteCartProduct = (req, res) => {
+export const deleteCartProduct =  (req, res) => {
   console.log(req.body);
   const { user_id, product_id } = req.body;
 
-  connection.query(
+ connection.query(
     `DELETE FROM CartProducts WHERE user_id = ${user_id} AND product_id = ${product_id}`,
     (err, results) => {
       if (!err) {
--- Controllers/getUserCartProducts.js
+++ Controllers/getUserCartProducts.js
@@ -1,9 +1,9 @@
 import connection from "../Database/connection.js";
 
-export const getUserCartProducts = (req, res) => {
+export const getUserCartProducts = async (req, res) => {
   const id = +req.params.id;
-  connection.query(
-    `SELECT products.product_id, products.name, products.Price, products.image, CartProducts.count FROM ((CartProducts INNER JOIN products ON CartProducts.product_id = products.product_id)
+ await connection.query(
+      `SELECT products.product_id, products.name, products.Price, products.image, CartProducts.count FROM ((CartProducts INNER JOIN products ON CartProducts.product_id = products.product_id)
   INNER JOIN users ON CartProducts.user_id = users.id) WHERE users.id = ${id}`,
     (err, results) => {
       if (!err) {
